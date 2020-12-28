1b485758a9358fdc2c174ecc411b5e2ca7cfd8e4
Stephen Makilan <stephensmakilan@gmail.com>
2020-12-28 11:56:14 +0800

    no

--- src/index.js
+++ src/index.js
@@ -3,18 +3,27 @@ import ReactDOM from 'react-dom';
 import './index.css';
 
 class Square extends React.Component {
+    constructor(props){
+      super(props);
+      this.state = {
+        value: null
+      };
+    }
+
     render() {
       return (
-        <button className="square">
-          {/* TODO */}
+        <button className="square" onClick={() => this.setState({
+          value: 'X'
+          })}>
+          {this.state.value}
         </button>
       );
     }
   }
   
-  class Board extends React.Component {
+class Board extends React.Component {
     renderSquare(i) {
-      return <Square />;
+      return <Square value={i}/>;
     }
   
     render() {
@@ -43,7 +52,7 @@ class Square extends React.Component {
     }
   }
   
-  class Game extends React.Component {
+class Game extends React.Component {
     render() {
       return (
         <div className="game">
